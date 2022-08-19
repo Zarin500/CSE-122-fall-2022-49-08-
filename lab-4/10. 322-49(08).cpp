7ae05125f9173e448f4fa14eb978c6001a973b50
Shohayel Ahmed <shohayel500@gmail.com>
2022-08-19 15:41:57 +0600

    Add files via upload

---
+++ lab-4/10. 322-49(08).cpp
@@ -0,0 +1,18 @@
+// Codeforces problem: 281A
+// Problem name: Word Capitalization
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    char s[200];
+    cin >> s;
+    if(s[0]>=97 && s[0]<=122){
+        s[0]=s[0]-32;
+    }
+    cout << s << endl;
+    return 0;
+}
